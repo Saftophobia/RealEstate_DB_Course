7e7291740ea88a8eaf62969ba6e346faa68d9bde
Saftophobia <asafty2003@gmail.com>
2015-04-13 21:15:50 +0200

    Person.java

---
+++ src/com/db/models/Person.java
@@ -0,0 +1,42 @@
+package com.db.models;
+
+/**
+ * Created by saftophobia on 4/13/15.
+ */
+public class Person {
+    private String FirstName;
+    private String Name;
+    private String Address;
+
+
+    
+
+
+
+
+
+    //setters and getters
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+}
