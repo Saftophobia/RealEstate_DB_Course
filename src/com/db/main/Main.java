3b78b3e4c41c1ab7206dc47eab2effe2759f76b9
Saftophobia <asafty2003@gmail.com>
2015-04-13 20:56:05 +0200

    reconstructing the Structure

---
+++ src/com/db/main/Main.java
@@ -0,0 +1,11 @@
+package com.db.main;
+
+/**
+ * Created by saftophobia on 4/13/15.
+ */
+public class Main {
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World");
+    }
+}
