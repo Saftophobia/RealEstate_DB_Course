3d1ef2f8c05c086b4085034c4df3aa3a709ba886
asaleh <3saleh@informatikum.uni-hamburg.de>
2015-04-21 22:38:36 +0200

    selection updated

--- db/Selections.sql
+++ db/Selections.sql
@@ -1,5 +1,11 @@
 Select Estate.ID,CITY ,POSTAL_CODE ,STREET,STREET_NUMBER,
-SQUARE_AREA ,EstateAgent_ID,FLOORS ,PRICE ,GARDEN 
+SQUARE_AREA ,FLOORS ,PRICE ,GARDEN 
+from Estate inner join House
+where Estate.ID=House.Estate_ID
+
+
+Select Estate.ID,CITY ,POSTAL_CODE ,STREET,STREET_NUMBER,
+SQUARE_AREA ,FLOOR,RENT,ROOMS,BALCONY,KITCHEN
 from Estate inner join House
 where Estate.ID=House.Estate_ID
 
