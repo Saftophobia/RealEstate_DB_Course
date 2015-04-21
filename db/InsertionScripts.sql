694905df4065a7f558119ab29fee8c7ee9c19e6e
asaleh <3saleh@informatikum.uni-hamburg.de>
2015-04-21 22:25:58 +0200

    Insertion Updated

--- db/InsertionScripts.sql
+++ db/InsertionScripts.sql
@@ -1,38 +1,38 @@
 INSERT INTO EstateAgent(NAME ,ADDRESS , LOGIN ,PASSWORD )
 values ('Ahmed Safty', 'Stellingon', 'Safty', 'hackable');
 
-INSERT INTO Estate
-values (1,'Hamburg','11242','Hamburg Main street','16','215',1);
+INSERT INTO Estate(CITY ,POSTAL_CODE ,STREET,STREET_NUMBER,SQUARE_AREA ,EstateAgent_ID )
+values ('Hamburg','11242','Hamburg Main street','16','215',1);
 
-INSERT INTO Estate
-values (2,'Hamburg','1112','Hamburg Main street','18','218',1);
+INSERT INTO Estate(CITY ,POSTAL_CODE ,STREET,STREET_NUMBER,SQUARE_AREA ,EstateAgent_ID )
+values ('Hamburg','1112','Hamburg Main street','18','218',1);
 
-INSERT INTO House
-values (1,2,750000,TRUE,1);
+INSERT INTO House (FLOORS ,PRICE ,GARDEN ,Estate_ID )
+values (2,750000,TRUE,1);
 
-INSERT INTO Apartment
-VALUES (1,12,750,5,FALSE,TRUE,2);
+INSERT INTO Apartment (FLOOR,RENT,ROOMS,BALCONY,KITCHEN, Estate_ID)
+VALUES (12,750,5,FALSE,TRUE,2);
 
-INSERT INTO Person
-Values (1,'Ali','Saleh','Borgfelder st, 16');
+INSERT INTO Person (FIRST_NAME ,NAME ,ADDRESS)
+Values ('Ali','Saleh','Borgfelder st, 16');
 
-INSERT INTO Person
-Values (2,'Ahmed','Saad','Borgfelder st, 16');
+INSERT INTO Person (FIRST_NAME ,NAME ,ADDRESS)
+Values ('Ahmed','Saad','Borgfelder st, 16');
 
-INSERT INTO PersonEstateRelation
-VALUES (1,1,1);
+INSERT INTO PersonEstateRelation (Person_ID ,Estate_ID )
+VALUES (1,1);
 
-INSERT INTO PersonEstateRelation
-VALUES (2,2,2);
+INSERT INTO PersonEstateRelation(Person_ID ,Estate_ID )
+VALUES (2,2);
 
-INSERT INTO Contract
-VALUES (1,1231,NOW(),'ST petrisburg',1);
+INSERT INTO Contract (CONTRACT_NUMBER,DATE ,PLACE ,PE_ID)
+VALUES (1231,NOW(),'ST petrisburg',1);
 
-INSERT INTO Contract
-VALUES (2,1232,NOW(),'ST petrisburg',2);
+INSERT INTO Contract(CONTRACT_NUMBER,DATE ,PLACE ,PE_ID)
+VALUES (1232,NOW(),'ST petrisburg',2);
 
-INSERT INTO PurchaseContract
-VALUES(1,1,12,1);
+INSERT INTO PurchaseContract (INSTALLMENTS, INTEREST_RATE , Contract_ID)
+VALUES(1,12,1);
 
-INSERT INTO TenancyContract
-VALUES (1,NOW(),2,20,2);
+INSERT INTO TenancyContract(START_DATE,DURATION ,ADD_COSTS,Contract_ID )
+VALUES (NOW(),2,20,2);
