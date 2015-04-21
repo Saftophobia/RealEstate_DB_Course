c238f1f874dfb37d0192b059e485d3037c07daec
asaleh <3saleh@informatikum.uni-hamburg.de>
2015-04-22 01:09:58 +0200

    readd insertions

--- db/InsertionScripts.sql
+++ db/InsertionScripts.sql
@@ -1,3 +1,23 @@
+INSERT INTO EstateAgent(NAME ,ADDRESS , LOGIN ,PASSWORD )
+values ('Ahmed Safty', 'Stellingon', 'Safty', 'hackable');
+
+INSERT INTO Estate(CITY ,POSTAL_CODE ,STREET,STREET_NUMBER,SQUARE_AREA ,EstateAgent_ID )
+values ('Hamburg','11242','Hamburg Main street','16','215',1);
+
+INSERT INTO Estate(CITY ,POSTAL_CODE ,STREET,STREET_NUMBER,SQUARE_AREA ,EstateAgent_ID )
+values ('Hamburg','1112','Hamburg Main street','18','218',1);
+
+INSERT INTO House (FLOORS ,PRICE ,GARDEN ,Estate_ID )
+values (2,750000,TRUE,1);
+
+INSERT INTO Apartment (FLOOR,RENT,ROOMS,BALCONY,KITCHEN, Estate_ID)
+VALUES (12,750,5,FALSE,TRUE,2);
+
+INSERT INTO Person (FIRST_NAME ,NAME ,ADDRESS)
+Values ('Ali','Saleh','Borgfelder st, 16');
+
+INSERT INTO Person (FIRST_NAME ,NAME ,ADDRESS)
+Values ('Ahmed','Saad','Borgfelder st, 16');
 
 INSERT INTO PersonEstateRelation (Person_ID ,Estate_ID )
 VALUES (1,1);
