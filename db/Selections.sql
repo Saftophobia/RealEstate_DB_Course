0d802b6900dd0b48c4177670ac638c8f3f812a7d
asaleh <3saleh@informatikum.uni-hamburg.de>
2015-04-22 00:46:46 +0200

    Selections the grat join

--- db/Selections.sql
+++ db/Selections.sql
@@ -10,3 +10,9 @@ from Estate inner join Apartment
 where Estate.ID=Apartment.Estate_ID
 
 
+SELECT CONTRACT_NUMBER ,DATE, PLACE ,INSTALLMENTS ,INTEREST_RATE ,FIRST_NAME ,
+	NAME ,CITY ,POSTAL_CODE ,STREET ,STREET_NUMBER ,SQUARE_AREA 
+from Contract inner join PurchaseContract on Contract_ID = Contract.ID
+inner join PersonEstateRelation on PersonEstateRelation.ID = PE_ID
+inner join Person on Person.ID = Person_ID
+inner join Estate on Estate.ID = PersonEstateRelation.Estate_ID
